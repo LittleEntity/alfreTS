45b26913bc49218b11d4ab9853927335bb4be640
Kajetan Weiß <k.weiss@hochschule-trier.de>
2014-02-11 23:30:15 +0100

    added testing.py script: written for testing

---
+++ testing.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+from AlfredFeedback import AlfredFeedback
+
+feedback = AlfredFeedback( )
+feedback.addItem( 1, 'FRST', 'first', 'First Item', '/path/anIcon.png')
+feedback.addItem( 2, 'SCND', 'second', 'Second Item', '/anotherPath/myIcon.png', 'file' )
+feedback.printFeedback( )
