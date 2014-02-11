da75ed5f42a327d9aa39b42d5285a64a838cbbdc
Kajetan Weiß <k.weiss@hochschule-trier.de>
2014-02-11 23:28:31 +0100

    reimplmented AlfredFeedback class with xml.etree.ElemenTree module

--- AlfredFeedback.py
+++ AlfredFeedback.py
@@ -1,49 +1,43 @@
-# -*- coding: utf-8 -*-
+# -*- coding: UTF-8 -*-
+import xml.etree.ElementTree as ET
+
 class AlfredFeedback:
-	feedbackDict = {}
-	def addItem(self, uniqueIdentifier, argument, title, subtitle = None, icon = None, itemtype = None):
-		self.assertUIdentIsNew(uniqueIdentifier)
-		self.assertValidItemType(itemtype)
-		item = dict( )
-		item['arg'] = argument
-		if itemtype is not None: item['type'] = itemtype
-		elementDict = dict( )
-		elementDict['title'] = title
-		if subtitle is not None: elementDict['subtitle'] = subtitle
-		if icon is not None: elementDict['icon'] = icon
-		item['elementDict'] = elementDict
-		self.feedbackDict[uniqueIdentifier] = item
+	itemsElement = ET.Element( 'items' )
 
-	def assertUIdentIsNew(self, uniqueIdentifier):
-		if uniqueIdentifier in self.feedbackDict:
-			raise NameError(uniqueIdentifier + ' was already added!')
+	def addItem( self, 
+	uniqueIdentifier, 
+	argument, 
+	title, 
+	subtitle = None, 
+	icon = None, 
+	itemtype = None ):
+		uniqueIdentifier = str( uniqueIdentifier )
+		self.assertUIdenIsNew( uniqueIdentifier )
+		self.assertValidItemType( itemtype )
+		itemElement = ET.SubElement( self.itemsElement, 'item' )
+		itemElement.set( 'uid', uniqueIdentifier )
+		itemElement.set( 'arg', argument )
+		if itemtype is not None: itemElement.set( 'type', itemtype )
+		titleElement = ET.SubElement( itemElement, 'title' )
+		titleElement.text = title
+		if subtitle is not None:
+			subtitleElement = ET.SubElement( itemElement, 'subtitle' )
+			subtitleElement.text = subtitle
+		if icon is not None:
+			iconElement = ET.SubElement( itemElement, 'icon' )
+			iconElement.text = icon
 
-	def assertValidItemType(self, itemtype):
+	def assertUIdenIsNew( self, uniqueIdentifier ):
+		for child in self.itemsElement:
+			if uniqueIdentifier == child.get( 'uid' ):
+				errorString = 'the identifier \"' + uniqueIdentifier + '\" was already added!'
+				raise NameError(errorString)
+
+	def assertValidItemType( self, itemtype ):
 		if itemtype is not None:
 			if itemtype != 'file':
 				raise Exception('The value of parameter itemtype must be a string containing "file"!')
 
-	def buildFeedbackXML(self):
-		xml = '<?xml version="1.0"?>'
-		xml += '\n'
-		xml += '<items>'
-		for (uid, item) in self.feedbackDict.viewitems( ):
-			xml += '\n\t'
-			xml += '<item uid="' + uid + '"'
-			for (attribute, value) in item.viewitems( ):
-				# if isinstance(value, str):
-				if attribute != 'elementDict':
-					xml += ' ' + attribute + '="' + value + '"'
-			xml += '>'
-			elementDict = item['elementDict']
-			for (element, value) in elementDict.viewitems( ):
-				xml += '\n\t\t'
-				xml += '<' + element + '>' + value + '</' + element + '>'
-			xml += '\n\t'
-			xml += '</item>'
-		xml += '\n'
-		xml += '</items>'
-		return xml
-
-	def writeFeedback(self):
-		print(self.buildFeedbackXML( ))
+	def writeFeedback( self ):
+		response = ET.tostring( self.itemsElement, 'UTF-8', 'xml' )
+		print( response )
