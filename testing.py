5c5ae132a6e0bca87980391cabbe97e220de899a
Kajetan Weiß <k.weiss@hochschule-trier.de>
2014-02-11 23:38:32 +0100

    fixed method naming

--- testing.py
+++ testing.py
@@ -4,4 +4,4 @@ from AlfredFeedback import AlfredFeedback
 feedback = AlfredFeedback( )
 feedback.addItem( 1, 'FRST', 'first', 'First Item', '/path/anIcon.png')
 feedback.addItem( 2, 'SCND', 'second', 'Second Item', '/anotherPath/myIcon.png', 'file' )
-feedback.printFeedback( )
+feedback.writeFeedback( )
