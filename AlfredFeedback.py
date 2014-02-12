438fd8e7b84faf2493d3140047c3255a927b9a80
Kajetan Weiß <k.weiss@hochschule-trier.de>
2014-02-12 03:32:25 +0100

    implemented workaround for UTF-8 letters; implemented unicode charakter support; redefined all strings as unicode strings;

--- AlfredFeedback.py
+++ AlfredFeedback.py
@@ -1,43 +1,47 @@
 # -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
 import xml.etree.ElementTree as ET
 
 class AlfredFeedback:
-	itemsElement = ET.Element( 'items' )
+	itemsElement = ET.Element( u'items' )
 
-	def addItem( self, 
-	uniqueIdentifier, 
-	argument, 
-	title, 
-	subtitle = None, 
-	icon = None, 
-	itemtype = None ):
+	def addItem(	
+			self, 
+			uniqueIdentifier, 
+			argument, 
+			title,
+			subtitle = None, 
+			icon = None, 
+			itemtype = None ):
 		uniqueIdentifier = str( uniqueIdentifier )
 		self.assertUIdenIsNew( uniqueIdentifier )
 		self.assertValidItemType( itemtype )
-		itemElement = ET.SubElement( self.itemsElement, 'item' )
-		itemElement.set( 'uid', uniqueIdentifier )
-		itemElement.set( 'arg', argument )
-		if itemtype is not None: itemElement.set( 'type', itemtype )
-		titleElement = ET.SubElement( itemElement, 'title' )
+		itemElement = ET.SubElement( self.itemsElement, u'item' )
+		itemElement.set( u'uid', uniqueIdentifier )
+		itemElement.set( u'arg', argument )
+		if itemtype is not None: itemElement.set( u'type', itemtype )
+		titleElement = ET.SubElement( itemElement, u'title' )
 		titleElement.text = title
 		if subtitle is not None:
-			subtitleElement = ET.SubElement( itemElement, 'subtitle' )
+			subtitleElement = ET.SubElement( itemElement, u'subtitle' )
 			subtitleElement.text = subtitle
 		if icon is not None:
-			iconElement = ET.SubElement( itemElement, 'icon' )
+			iconElement = ET.SubElement( itemElement, u'icon' )
 			iconElement.text = icon
 
 	def assertUIdenIsNew( self, uniqueIdentifier ):
 		for child in self.itemsElement:
-			if uniqueIdentifier == child.get( 'uid' ):
-				errorString = 'the identifier \"' + uniqueIdentifier + '\" was already added!'
-				raise NameError(errorString)
+			if uniqueIdentifier == child.get( u'uid' ):
+				errorString = ( u'the identifier \"' + uniqueIdentifier 
+					+ u'\" was already added!' )
+				raise NameError( errorString )
 
 	def assertValidItemType( self, itemtype ):
 		if itemtype is not None:
-			if itemtype != 'file':
-				raise Exception('The value of parameter itemtype must be a string containing "file"!')
+			if itemtype != u'file':
+				raise Exception( u'The value of parameter itemtype' 
+					u'must be a string containing "file"!' )
 
 	def writeFeedback( self ):
-		response = ET.tostring( self.itemsElement, 'UTF-8', 'xml' )
-		print( response )
+		response = ET.tostring( self.itemsElement )
+		print( response.encode( 'UTF-8' ) )
--- testing.py
+++ testing.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
 from AlfredFeedback import AlfredFeedback
 
 feedback = AlfredFeedback( )
-feedback.addItem( 1, 'FRST', 'first', 'First Item', '/path/anIcon.png')
-feedback.addItem( 2, 'SCND', 'second', 'Second Item', '/anotherPath/myIcon.png', 'file' )
+feedback.addItem( 1, u'FRST', u'first', u'First Item', u'/path/anIcon.png')
+feedback.addItem( 2, u'SCND', u'second', u'Second Item', u'/anotherPath/myIcon.png', u'file' )
+feedback.addItem( 3, u'THRD', u'ßpêcîâl', u'', u'/wherever/superIcon.png', u'file' )
 feedback.writeFeedback( )
