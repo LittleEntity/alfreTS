e35c783d99d4f812475a6d568a99525e9d33d22f
Kajetan Weiß <k.weiss@hochschule-trier.de>
2014-02-14 21:51:15 +0100

    quickly added file execution in testing

--- testing.py
+++ testing.py
@@ -7,3 +7,15 @@ feedback.addItem( 1, u'FRST', u'first', u'First Item', u'/path/anIcon.png')
 feedback.addItem( 2, u'SCND', u'second', u'Second Item', u'/anotherPath/myIcon.png', u'file' )
 feedback.addItem( 3, u'THRD', u'ßpêcîâl', u'', u'/wherever/superIcon.png', u'file' )
 feedback.writeFeedback( )
+
+myLocale = ('el_GR', 'UTF-8')
+#myLocale = ('de_DE', 'UTF-8')
+formatList = [
+	("%Y%m%d%H%M", "YearMonthDayHourMinute for versioning"),
+	("%x %X", "Date and time representation appropriate to locale setting"),
+	#("%A %d.%b.%Y um %H:%M Uhr", "German date representation"),
+	("%A, %B %d, %Y at %Hh:%M %p", "US date representation"),
+]
+
+execfile("main.py")
+main(formatList)
